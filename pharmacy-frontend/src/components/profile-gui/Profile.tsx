13edbaf8fbc22a82334815a41c8f228ac8ac1104
phoenixrgarcia <140856571+phoenixrgarcia@users.noreply.github.com>
2024-11-06 14:31:06 -0700

    profile empty

---
+++ pharmacy-frontend/src/components/profile-gui/Profile.tsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  Container,
+  AppBar,
+  Toolbar,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
