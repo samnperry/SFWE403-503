4a14ac3a335aa8bcdb869487428ee0eda6d49592
WilliamRains <30558062+WilliamRains@users.noreply.github.com>
2024-11-25 14:51:31 -0700

    Pharm

--- pharmacy-frontend/src/components/pharm-gui/Pharm.tsx
+++ pharmacy-frontend/src/components/pharm-gui/Pharm.tsx
@@ -27,10 +27,7 @@ function Pharm() {
   const [signatureData, setSignatureData] = useState<string | null>(null);
   const sigPad = useRef<SignatureCanvas>(null);
 
-  const handleNavigateHome = () => {
-    navigate("/Pharm");
-  };
-
+  const handleNavigateHome = () => navigate("/Pharm");
   const handleLogout = async () => {
     try {
       const response = await fetch("http://localhost:5001/api/logout", {
@@ -40,31 +37,21 @@ function Pharm() {
         },
         body: JSON.stringify({ user }),
       });
-
       if (response.ok) {
         const data = await response.json();
-        console.log(data.message);
         window.location.href = data.redirect;
       } else {
         const errorData = await response.json();
-        console.error("Logout failed:", errorData.error);
         alert(errorData.error);
       }
     } catch (error) {
-      console.error("Error during logout:", error);
       alert("An unexpected error occurred. Please try again.");
     }
   };
 
   const handleProfile = () => navigate("/ProfilePage");
-
-  const handleOpenSignaturePad = () => {
-    setOpenSignaturePad(true);
-  };
-
-  const handleCloseSignaturePad = () => {
-    setOpenSignaturePad(false);
-  };
+  const handleOpenSignaturePad = () => setOpenSignaturePad(true);
+  const handleCloseSignaturePad = () => setOpenSignaturePad(false);
 
   const handleSaveSignature = () => {
     if (sigPad.current && sigPad.current.isEmpty()) {
@@ -72,158 +59,160 @@ function Pharm() {
     } else if (sigPad.current) {
       setSignatureData(sigPad.current.getTrimmedCanvas().toDataURL("image/png"));
       setOpenSignaturePad(false);
-      // You can handle the saved signature data here
     }
   };
 
-  const handleClearSignature = () => {
-    if (sigPad.current) {
-      sigPad.current.clear();
-    }
-  };
+  const handleClearSignature = () => sigPad.current?.clear();
+
+
+  const handleNavigatePharmacistInventory = () => navigate("/PharmacistInventory");
+  const handleNavigatePatientManager = () => navigate("/PatientManager");
+  const handleNavigateCashier = () => navigate("/Cashier");
 
   return (
-    <div className="pharm-background">
-      <Box
-        className="pharm-background"
-        sx={{
-          backgroundColor: "#f7f9fc",
-          minHeight: "100vh",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <AppBar position="fixed">
-          <Toolbar>
-            <Typography variant="h6" sx={{ flexGrow: 1 }}>
-              Pharmacy Page
-            </Typography>
-            <Button color="inherit" onClick={handleNavigateHome}>
-              Home
-            </Button>
-            <Button color="inherit" onClick={handleProfile}>
-              Profile
-            </Button>
-            <Button color="inherit" onClick={handleLogout}>
-              Log Out
-            </Button>
-          </Toolbar>
-        </AppBar>
+    <Box
+      className="pharm-background"
+      sx={{
+        backgroundColor: "#f7f9fc",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      {/* AppBar */}
+      <AppBar position="fixed" sx={{ backgroundColor: "#00796b" }}>
+        <Toolbar sx={{ display: "flex", justifyContent: "center" }}>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Pharmacy Dashboard
+          </Typography>
+          <Button color="inherit" onClick={handleNavigateHome}>
+            Home
+          </Button>
+          <Button color="inherit" onClick={handleProfile}>
+            Profile
+          </Button>
+          <Button color="inherit" onClick={handleLogout}>
+            Log Out
+          </Button>
+        </Toolbar>
+      </AppBar>
 
-        {/* Spacer to prevent content from being hidden behind the AppBar */}
-        <Toolbar />
-
-        <Container
+      {/* Main Content */}
+      <Container
         maxWidth="lg"
         sx={{
           marginTop: "6rem",
-          padding: "2rem",
           flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
-          {/* Ensure the grid is at the top */}
-          <Box mt={2} sx={{ flexGrow: 1 }}>
-            <Grid container spacing={4}>
-              <Grid item xs={12} sm={6} md={4}>
-                <Card>
+        <Box
+          sx={{
+            background: "white",
+            width: "100%",
+            borderRadius: 2,
+            padding: "2rem",
+            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <Typography variant="h5" sx={{ fontWeight: "bold", marginBottom: "2rem" }}>
+            Welcome to the Pharmacy System
+          </Typography>
+          <Grid container spacing={4}>
+            <Grid item xs={12} sm={6} md={6}>
+              <Card sx={{ boxShadow: 3, "&:hover": { boxShadow: 6 } }}
+                onClick={handleNavigatePharmacistInventory}>
+                <CardActionArea>  
                   <CardContent>
-                    <Typography gutterBottom variant="h6" component="div">
+                    <Typography variant="h6" gutterBottom>
                       Manage Inventory
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                      Keep track of medications and supplies with our efficient
-                      inventory system.
+                      Keep track of medications and supplies with our efficient inventory system.
                     </Typography>
                   </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} sm={6} md={4}>
-                <Card>
+                </CardActionArea>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6}>
+              <Card sx={{ boxShadow: 3, "&:hover": { boxShadow: 6 } }}
+                onClick={handleNavigatePatientManager}>
+                <CardActionArea>
                   <CardContent>
-                    <Typography gutterBottom variant="h6" component="div">
+                    <Typography variant="h6" gutterBottom>
                       Patient Records
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                      Access and manage patient information securely and
-                      conveniently.
+                      Access and manage patient information securely and conveniently.
                     </Typography>
                   </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} sm={6} md={4}>
-                <Card>
+                </CardActionArea>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6}>
+              <Card sx={{ boxShadow: 3, "&:hover": { boxShadow: 6 } }}
+                onClick={handleNavigateCashier}> 
+                <CardActionArea>
                   <CardContent>
-                    <Typography gutterBottom variant="h6" component="div">
-                      Analytics
+                    <Typography variant="h6" gutterBottom>
+                      Cashier
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                      Gain insights with our comprehensive analytics tools.
+                      Checkout a customer for either OTC items or prescriptions.
                     </Typography>
                   </CardContent>
-                </Card>
-              </Grid>
-              {/* New Add Signature Card */}
-              <Grid item xs={12} sm={6} md={4}>
-                <Card>
-                  <CardActionArea onClick={handleOpenSignaturePad}>
-                    <CardContent>
-                      <Typography gutterBottom variant="h6" component="div">
-                        Add Signature
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        Draw and save your signature for prescriptions and
-                        documents.
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-              </Grid>
+                </CardActionArea>
+              </Card>
             </Grid>
-          </Box>
-        </Container>
-      </Box>
+            <Grid item xs={12} sm={6} md={6}>
+              <Card
+                sx={{ boxShadow: 3, "&:hover": { boxShadow: 6 } }}
+                onClick={handleOpenSignaturePad}
+              >
+                <CardActionArea>
+                  <CardContent>
+                    <Typography variant="h6" gutterBottom>
+                      Add Signature
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Draw and save your signature for prescriptions and documents.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+
 
       {/* Signature Pad Dialog */}
-      <Dialog
-        open={openSignaturePad}
-        onClose={handleCloseSignaturePad}
-        maxWidth="sm"
-        fullWidth
-      >
+      <Dialog open={openSignaturePad} onClose={handleCloseSignaturePad} maxWidth="sm" fullWidth>
         <DialogTitle>Draw Your Signature</DialogTitle>
-        <DialogContent>
+        <DialogContent sx={{ textAlign: "center" }}>
           <SignatureCanvas
             penColor="black"
-            canvasProps={{ width: 500, height: 200, className: "sigCanvas" }}
+            canvasProps={{
+              width: 500,
+              height: 200,
+              className: "sigCanvas",
+            }}
             ref={sigPad}
           />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClearSignature}>Clear</Button>
           <Button onClick={handleCloseSignaturePad}>Cancel</Button>
-          <Button
-            onClick={handleSaveSignature}
-            variant="contained"
-            color="primary"
-          >
+          <Button onClick={handleSaveSignature} variant="contained" color="primary">
             Save
           </Button>
         </DialogActions>
       </Dialog>
-
-      {/* Footer */}
-      <footer
-        style={{
-          textAlign: "center",
-          padding: "1rem",
-          backgroundColor: "#f1f1f1",
-        }}
-      >
-        <Typography variant="body2" color="textSecondary">
-          &copy; 2024 Pharmacy System. All rights reserved.
-        </Typography>
-      </footer>
-    </div>
+      
+    </Box>
+    
   );
 }
 
